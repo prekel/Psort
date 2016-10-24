62943e7b8a214ec140de8fb6678dd9566d04b5aa
Владислав Прекель <79232809414@yandex.ru>
2016-10-24 20:51:49 +0700

    add Psort

--- Psort/Psort/main.cpp
+++ Psort/Psort/main.cpp
@@ -8,14 +8,17 @@
 
 using namespace std;
 
-const int m = 100;
-int a[m], i, b[m];
+const int m = 100000;
+int a[m], i, b[m], pqs;
+long t0, t;
+
+int Psort(int l, int r, int s);
 
 int quicksort(int l, int r, int s)
 {
 	int i = 0, j = r - 1, x, mid;
 
-	cerr << "quicksort         " << setw(6) << l << setw(6) << r << setw(3) << s << endl;
+	//cerr << "quicksort         " << setw(7) << l << setw(7) << r << setw(7) << r - l << setw(4) << s << endl;
 
 	mid = (l + r) / 2;
 	x = a[mid];
@@ -35,8 +38,8 @@ int quicksort(int l, int r, int s)
 		}
 	}
 
-	if (j > l) quicksort(l, j, s + 1);
-	if (r > i) quicksort(i, r, s + 1);
+	if (j > l) Psort(l, j, s + 1);
+	if (r > i) Psort(i, r, s + 1);
 	return 0;
 }
 
@@ -46,11 +49,11 @@ int mergesort(int l, int r, int s)
 
 	int m, k, i, j;
 
-	cerr << "mergesort         " << setw(6) << l << setw(6) << r << setw(3) << s << endl;
+	//cerr << "     mergesort    " << setw(7) << l << setw(7) << r << setw(7) << r - l << setw(4) << s << endl;
 
 	m = (l + r) / 2;
-	mergesort(l, m, s + 1);
-	mergesort(m + 1, r, s + 1);
+	Psort(l, m, s + 1);
+	Psort(m + 1, r, s + 1);
 	k = l; i = l; j = m + 1;
 	while (k <= r)
 		if (i <= m && (j > r || a[i] < a[j]))
@@ -67,8 +70,8 @@ int priorityqueuesort(int l, int r, int s)
 	priority_queue <int> h;
 	int i;
 
-	cerr << "priorityqueuesort " << setw(6) << l << setw(6) << r << setw(3) << s << endl;
-	
+	//cerr << "priorityqueuesort " << setw(7) << l << setw(7) << r << setw(7) << r - l << setw(4) << s << endl;
+
 	for (i = l; i <= r; i++)
 	{
 		h.push(a[i]);
@@ -81,16 +84,30 @@ int priorityqueuesort(int l, int r, int s)
 	return 0;
 }
 
+int Psort(int l, int r, int s)
+{
+	if (r - l <= pqs)
+		priorityqueuesort(l, r, s);
+	else if (rand() % 2)
+		quicksort(l, r, s);
+	else
+		mergesort(l, r, s);
+	return 0;
+}
+
 int main()
 {
 	srand(time(0));
+	pqs = min(max(10, m / 100), 512);
 
 	for (i = 0; i < m; i++)
 	{
 		a[i] = rand();
 	}
 
-	priorityqueuesort(0, m, 0);
+	t0 = clock();
+	Psort(0, m, 0);
+	t = clock() - t0;
 
 	return 0;
 }
